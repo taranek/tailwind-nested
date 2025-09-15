de07ab95fbabf83a9e8dc7ab93ca77f5abba60c9
taranek <t.taranek27@gmail.com>
2025-09-15 23:04:39 +0200

    chore(): link v3 playground (doesn't work yet)

--- examples/v3/src/App.tsx
+++ examples/v3/src/App.tsx
@@ -1,11 +1,20 @@
+import { twn } from 'tailwind-nested';
+
 function App() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center bg-green-800">
-      <h1 className="text-3xl font-bold underline text-green-200">
+    <div className={twn('w-screen h-screen flex items-center justify-center bg-green-800', {
+      hover: 'bg-green-900',
+      'dark': 'bg-slate-900'
+    })}>
+      <h1 className={twn('text-3xl font-bold underline text-green-200', {
+        hover: 'text-green-100 scale-105',
+        focus: 'ring-2 ring-green-400',
+        'dark': 'text-slate-200',
+        'dark:hover': 'text-white'
+      })}>
         Hello world!
       </h1>
     </div>
-
   )
 }
 
