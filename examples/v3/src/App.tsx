b070ee1b7a60b87df10eebef8776e9cd20f9ea92
taranek <t.taranek27@gmail.com>
2025-09-15 22:45:54 +0200

    chore(): add v3 playground

---
+++ examples/v3/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-green-800">
+      <h1 className="text-3xl font-bold underline text-green-200">
+        Hello world!
+      </h1>
+    </div>
+
+  )
+}
+
+export default App
