9f5eb72d4f852e613ccffc2a99c471a09cca82cb
taranek <t.taranek27@gmail.com>
2025-09-16 00:10:34 +0200

    chore(): prototype using transfomr

--- examples/v4/src/App.tsx
+++ examples/v4/src/App.tsx
@@ -3,14 +3,12 @@ import { twn } from 'tailwind-nested';
 function App() {
   return (
     <div className={twn('w-screen h-screen flex items-center justify-center bg-green-800', {
-      hover: 'bg-green-900',
-      'dark': 'bg-slate-900'
+      hover: 'bg-green-900 uppercase',
     })}>
       <h1 className={twn('text-3xl font-bold underline text-green-200', {
-        hover: 'text-green-100 scale-105',
+        hover: 'text-green-100 scale-105 border-2 border-red-200',
         focus: 'ring-2 ring-green-400',
-        'dark': 'text-slate-200',
-        'dark:hover': 'text-white'
+
       })}>
         Hello world!
       </h1>
